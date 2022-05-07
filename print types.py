1b907e08f2ea9209ea86147b2cd75501593bd410
ayesha-syeda <105065348+ayesha-syeda@users.noreply.github.com>
2022-05-07 21:35:33 +0530

    Add files via upload

---
+++ print types.py
@@ -0,0 +1,48 @@
+# print('hello world')
+# print("hello python")
+# print('''iam ayesha''')
+# print("""how are you""")
+
+# print("""hello
+# iam ayesha here
+# what are youn doing my dear""")
+# print('''hi 
+# my dear friend
+# where are u''')
+
+
+# print("i'm shainaz")
+# print("""i'm shainaz""")
+# print('''i'm shainaz''')
+# print('i\'m shainaz')
+
+
+#write a message on screen *python(world)*
+# print('python(world)')
+# print("python(world)")
+# print('''python(world)''')
+# print("""python(world)""")
+
+
+#write a message on screen *my fav sub is "maths"*
+# print('''my fav sub is" maths"''')
+# print(' my fav sub is "maths"')
+
+# *i'm from vzg*
+# print('i\'m from vzg')
+# print("i'm from vzg")
+# print("""i'm from vzg""")
+# print('''i'm from vzg'''')
+
+# *i'm going to london*
+# print('i\'m going to london')
+# print('''i'm going to london''')
+# print("i'm going to london")
+
+
+#we are leaving in a joint family#
+# print('we are leaving in a joint family')
+# print("we are leaving in a joint family")
+# print('''we are leaving in a joint family''')
+# print("""we are leaving in a joint family""")
+
